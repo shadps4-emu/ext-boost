f6c0e510cbeb37113b8091043aa2bc6cceb75f5e
Frodo Baggins <baggins31084@proton.me>
2024-10-08 15:29:39 -0700

    add align library (header only). Init CmakeLists.txt and add align include dir to include_directories

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 bcp ^
     boost/asio/asio.hpp ^
+    boost/container/flat_map.hpp ^
     boost/container/flat_set.hpp ^
     boost/container/small_vector.hpp ^
     boost/container/static_vector.hpp ^
@@ -8,4 +9,8 @@ bcp ^
     boost/icl/separate_interval_set.hpp ^
     boost/intrusive/list.hpp ^
     boost/intrusive/set.hpp ^
+    libs/align/include/boost/align/aligned_alloc.hpp ^
+    libs/align/include/boost/align/aligned_allocator.hpp ^
+    libs/align/include/boost/align/aligned_delete.hpp ^
+    align ^
     --boost="%1" .
